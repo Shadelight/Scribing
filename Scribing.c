a72158fc4425615a3941fc796870e5f5103115c2
Alberth Salazar <alberth.slzr@gmail.com>
2018-04-03 22:24:50 -0400

    Nuevo archivo
    Ya puede copiar el contenido de archivo temporal al nuevo archivo

--- Scribing.c
+++ Scribing.c
@@ -1,19 +1,29 @@
-/* *************************************************************************
- * Scribing  -- Un  simple editor de texto con la opción de poder corregir *
- *         palabras con mala ortografía y la capacidad para crear y        *
- *         modificar un archivo tipo odt ó txt.                            *
- *         proyecto de la asignatura: Técnica de Programación 1            *
- **************************************************************************/
-//----- Librerias Incluidas 
+//==================================================== archivo = Scribing.c ====
+//= Scribing - Editor de texto                                                 =
+//==============================================================================
+//   Scribing es un n  simple editor de texto con la opción de poder corregir  =
+//   palabras con mala ortografía y la capacidad para crear y modificar        =
+//   archivos de tipo .odt ó txt.                                              =
+//   Proyecto de la asignatura: Técnica de Programación 1                      =                                   =
+//==============================================================================
+//----- Librerias Incluidas ----------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-//----- Variables globales 
+//----- Constantes definidas ---------------------------------------------------
+#ifdef _WIN32
+#define  limpiar  system("cls");
+#endif
+
+#ifdef linux
+#define limpiar   system("clear");
+#endif
+
+//----- Variables globales -----------------------------------------------------
 char ruta_dicc[] = "Diccionario.txt";
 char ruta_asccii[] = "Ascii.txt";
 char ruta_leame[] = "Léame.md";
-char linea[] = "--------------------------------------------------------------------------------";
 FILE *pntr_leame;
 FILE *pntr_asccii;
 FILE *puntr_dicc;
@@ -37,9 +47,8 @@ void add_dicc();
 void ver_diccionario();
 void Leame();
 
-//----- Programa principal 
+//----- Programa principal -----------------------------------------------------
 int main() { 
-
 	do { 
 		menu();
 		scanf("%d", &opcion);
@@ -47,82 +56,64 @@ int main() {
 		op_menu(opcion);
 	} while(opcion != 5);
 
-	// limpiar
-	//fcloseall();
 	return 0;
 }
 
 //----- Cabecera ascii ---------------------------------------------------------
 void ascii_art() {
+	// definir
 	pntr_asccii = fopen(ruta_asccii,"r");
 	if (pntr_asccii == NULL) {
     	printf("\nError al cargar la cabecera (%s) ascii del programa. \n\n", ruta_asccii);
     } 
     else {
+    	// imprimir por pantalla los caracteres del archivo cabecera
     	while((caracter = fgetc(pntr_asccii)) != EOF) {
             printf("%c", caracter);
         }
-    fclose(pntr_asccii);
+    //fclose(pntr_asccii);
     }
 }
 
 //----- Menu principal ---------------------------------------------------------
 void menu() {
-	system("clear");
+	limpiar;
 	ascii_art();
 	printf("Menu Principal\n");
-	printf("%s\n", linea);
+	printf("--------------------------------------------------------------------------------\n");
 	printf("1. Archivo  |  2. Corrector | 3. Diccionario |  4. Acerca de   |  5. Salir\n");
-	printf("%s\n", linea);
+	printf("--------------------------------------------------------------------------------\n");
 	printf("\n>> ");
 }
 
+//----- Opciones del menu principal --------------------------------------------
 void op_menu(int op) {
 	switch(op) { 
-			case 1:
-				menu_archivo();
-				break;
-			case 2: 
-				corrector();
-				break;
-			case 3:
-				menu_diccionario();
-				break;
-			case 4:
-				Leame();
-				getchar();
-				break;
-			case 5:
-				break;
+			case 1: menu_archivo(); 	break;
+			case 2: corrector(); 		break;
+			case 3: menu_diccionario(); break;
+			case 4: Leame(); getchar(); break;
+			case 5:	/* salir */		    break;
 		} 
 }
 
-//----- Menu Archivo ---------------------------------------------------------
+//----- Menu Archivo -----------------------------------------------------------
 void menu_archivo() {
-	do{ 
-		system("clear");
+	do { 
+		limpiar;
 		ascii_art();
 		printf("Menu Archivo\n");
-		printf("%s\n", linea );
+		printf("--------------------------------------------------------------------------------\n");
 		printf("1. Nuevo  |  2. Abrir  |  3. Regresar  \n");
-		printf("%s\n", linea );
+		printf("--------------------------------------------------------------------------------\n");
 		printf("\n>> ");
 		scanf("%d", &opcion);
 		while (getchar() != '\n');
-
+		
 		switch(opcion) {
-			case 1:
-				system("clear");
-				ascii_art();
-				nuevo_archivo();
-				getchar();
-				break;
-			case 2:
-				abiri_archivo();
-				getchar();
-				break;
-			case 3:
-				break;
+			case 1: nuevo_archivo(); getchar(); break;
+			case 2: abiri_archivo(); getchar(); break;
+			case 3: /* regresar */              break;
 			default:
 				printf("Opcion incorrecta.\n");
 				break;
@@ -130,74 +121,93 @@ void menu_archivo() {
 	} while( opcion != 3);
 }
 
+//----- Crear nuevo archivo ----------------------------------------------------
 void nuevo_archivo() {
-	FILE *temp;
+	limpiar;
+	ascii_art();
+	FILE *temporal;
+	
 	char letras, nombre[60], tipo[6], nom_compl[100];
-
-	temp = fopen("temp.txt","w");
+	temporal = fopen("temporal.txt","w");
 
 	printf("Nuevo archivo\n");
 	printf("--------------------------------------------------------------------------------\n");
-	printf("Ingrese el contenido que desee\n");
+	printf("Ingrese el contenido para su archivo\n");
+	printf("(pulse ENTER para guardar)\n");
 	printf("--------------------------------------------------------------------------------\n");
-	printf("(presione ENTER para guardar)\n");
  	printf("\n~ ");
-
-		while((letras = getchar()) != '\n')
- 		{
- 			fputc(letras, temp);
- 		}
-
-	system("clear");
+ 	// escribir datos en archivo temporal hasta pulsar enter
+	while((letras = getchar()) != '\n')
+ 	{ 
+		fputc(letras, temporal); 
+	}
+	limpiar;
 	ascii_art();
 	printf("Nuevo archivo\n");
 	printf("--------------------------------------------------------------------------------\n");
  	printf(">> Gurdar Como:\n");
 	printf("--------------------------------------------------------------------------------\n");
- 	printf("\nNombre:\n");
- 	printf(">> ");
+ 	printf("\nNombre >> ");
+ 	//leer nombre
  	fgets(nombre, sizeof(nombre), stdin);
+ 	// eliminar espacio capturado en fgets
 	nombre[strlen(nombre) - 1] = '\0';
- 	printf("\n\nTipo: \n");
- 	printf("(.txt o .odt)\n");
- 	printf(">> ");
+ 	printf("\n\nTipo (.txt o .odt) >> ");
+ 	// leer la extencion
  	fgets(tipo, sizeof(tipo), stdin);
+ 	// eliminar espacio capturado en fgets
 	tipo[strlen(tipo) - 1] = '\0';
 
- 	// Concatenar el nombre del archivo con el tipo
+ 	// concatenar nombre + tipo
     strcpy( nom_compl, nombre );
     strcat( nom_compl, tipo );
-
+    // definir nuevo archivo con la concatenacion
     nuevo_arch = fopen(nom_compl,"w");
+    // cerrar temporal para reabrirlo mas tarde
+	fclose(temporal);
 
     if(nuevo_arch == NULL) {
-		printf("\n\nNo se pudo crear el archivo . . .");
+    	limpiar;
+    	ascii_art();
+    	printf("Nuevo archivo\n");
+    	printf("--------------------------------------------------------------------------------\n");
+		printf("\nNo se pudo crear el archivo . . .");
+		printf("--------------------------------------------------------------------------------\n");
 	}
 	else {
-
-    	system("clear");
+		// reabrir temporal, pero ahora en modo lectura
+		temporal = fopen("temporal.txt", "r");
+		// copiar contenido de temporal al nuevo archivo
+		while((caracter=fgetc(temporal))!=EOF && !ferror(temporal) && !ferror(nuevo_arch))
+		fputc(caracter,nuevo_arch);
+    	limpiar;
 		ascii_art();
 		printf("Nuevo archivo:\n");
 		printf("--------------------------------------------------------------------------------\n");
  		printf(">> Gurdar Como\n");
 		printf("--------------------------------------------------------------------------------\n");
-		printf("\nSu archivo (%s) se ha guardado con exito . . .", nom_compl);
-		fclose(temp);
+
+		printf("\nEl archivo (%s) ha sido creado y guardado con exito . . .", nom_compl);
+		// cerrar los archivos
+		fclose(temporal);
 		fclose(nuevo_arch);
-		remove("temp.txt");
+		// eliminar archivo temporal
+		remove("temporal.txt");
     }
 
 }
-
+//----- Leer un archivo --------------------------------------------------------
 void abiri_archivo() {
-	system("clear");
+	limpiar;
 	ascii_art();
+	
 	char  nombre[60], tipo[6], nom_compl[100];
-	printf("Menu Archivo\n");
+	
+	printf("Abrir Archivo\n");
 	printf("--------------------------------------------------------------------------------\n");
-	printf("Ingrese el nombre + el tipo del archivo que desea abrir:\n");
+	printf("Ingrese el nombre y luego el tipo del archivo que desea abrir:\n");
 	printf("--------------------------------------------------------------------------------\n");	
-	 printf("\nNombre:\n");
+	printf("\nNombre:\n");
  	printf(">> ");
  	fgets(nombre, sizeof(nombre), stdin);
 	nombre[strlen(nombre) - 1] = '\0';
@@ -210,23 +220,26 @@ void abiri_archivo() {
  	// Concatenar el nombre del archivo con el tipo
     strcpy( nom_compl, nombre );
     strcat( nom_compl, tipo );
-
+    // abrir archivo en modo lectura
     abrir_arch = fopen(nom_compl,"r");
 
-	char caracter;
-
 	if(abrir_arch == NULL) {
-		printf("\nNo se pudo abrir el archivo . . .");
+		limpiar;
+		ascii_art();
+		printf("Nuevo Archivo\n");
+		printf("--------------------------------------------------------------------------------\n");
+		printf("No se pudo abrir el archivo . . .");
+		printf("--------------------------------------------------------------------------------\n");
 	}
 	else {
-		system("clear");
+		limpiar;
 		ascii_art();
-		printf("%s\n", nom_compl);
+		printf(">> %s\n", nom_compl);
 		printf("--------------------------------------------------------------------------------\n");
-		printf("(presione ENTER para regresar)\n");
+		printf("(pulse ENTER para regresar)\n");
 		printf("--------------------------------------------------------------------------------\n");
 		printf("\n~ ");
-
+		// imprimir caracter a caracter los datos del archivo
         while((caracter = fgetc(abrir_arch)) != EOF) {
 			printf("%c",caracter);
 	    }
@@ -238,7 +251,7 @@ void abiri_archivo() {
 //----- Corrector Ortografico -----------------------------------------------------
 void corrector() {
 		do{ 
-		system("clear");
+		limpiar;
 		ascii_art();
 		printf("Corrector Ortografico\n");
 		printf("--------------------------------------------------------------------------------\n");
@@ -250,7 +263,7 @@ void corrector() {
 
 		switch(opcion) {
 			case 1:
-				system("clear");
+				limpiar;
 				ascii_art();
 				corregir_texto();
 				getchar();
@@ -269,7 +282,7 @@ void corrector() {
 void corregir_texto() {
 	printf("Corrector Ortografico\n");
 	printf("--------------------------------------------------------------------------------\n");
-	printf("Funcion en desarrollo...\n");
+	printf("Funcion en desarrollo . . .\n");
 	printf("--------------------------------------------------------------------------------\n");
 	printf("\n>> ");
 }
@@ -278,7 +291,7 @@ void corregir_texto() {
 
 void menu_diccionario(){
 	do{
-		system("clear");
+		limpiar;
 		ascii_art();
 		printf("Menu Diccionario\n");
 		printf("--------------------------------------------------------------------------------\n");
@@ -291,13 +304,13 @@ void menu_diccionario(){
 		switch(opcion)
 		{
 			case 1:
-				system("clear");
+				limpiar;
 				ascii_art();
 				add_dicc();
 				getchar();
 				break; 
 			case 2:
-				system("clear");
+				limpiar;
 				ascii_art();
 				ver_diccionario();
 				getchar();
@@ -306,7 +319,7 @@ void menu_diccionario(){
 				break;
 			default:
 				printf("Opcion incorrecta.\n");
-				getchar();
+				getchar(); 
 				break;
 		}
 	}while(opcion !=3);
@@ -320,12 +333,12 @@ void add_dicc(){
     {
     	printf("Menu Diccionario\n");
     	printf("--------------------------------------------------------------------------------\n");
-    	printf("ERROR al abrir (%s)\n", ruta_dicc);
+    	printf("ERROR al cargar (%s)\n", ruta_dicc);
     	printf("--------------------------------------------------------------------------------\n");
     }
     else
     {	
-    	system("clear");
+    	limpiar;
     	ascii_art();
     	printf("Menu Diccionario\n");
     	printf("--------------------------------------------------------------------------------\n");
@@ -345,13 +358,14 @@ void add_dicc(){
 
 // abrir archivo de diccionario
 void ver_diccionario() {
+	// definir modo lectura  
 	puntr_dicc = fopen(ruta_dicc,"r");
 
 	if (puntr_dicc == NULL)
     {
     	printf("Menu Diccionario\n");
     	printf("--------------------------------------------------------------------------------\n");
-    	printf("ERROR al abrir (%s)\n", ruta_dicc);
+    	printf("ERROR al cargar (%s)\n", ruta_dicc);
     	printf("--------------------------------------------------------------------------------\n");
     }
     else
@@ -371,7 +385,7 @@ void ver_diccionario() {
 
 //----- Acerca del programa ----------------------------------------------------
 void Leame() {
-	system("clear");
+	limpiar;
 	ascii_art();
 
 	pntr_leame = fopen(ruta_leame,"r");
